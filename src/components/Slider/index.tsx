5870b3beca3985afde7b34b54716732e46bb584a
dinh.thi.thu.ha <dinh.thi.thu.ha@sun-asterisk.com>
2024-05-10 18:11:04 +0700

    80% slider

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -6,8 +6,18 @@ import NotListedLocationOutlinedIcon from '@mui/icons-material/NotListedLocation
 import EventAvailableOutlinedIcon from '@mui/icons-material/EventAvailableOutlined';
 import Groups2OutlinedIcon from '@mui/icons-material/Groups2Outlined';
 import ZoomInOutlinedIcon from '@mui/icons-material/ZoomInOutlined';
+import Input from "@mui/material/Input";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+import { SingleInputDateRangeField } from '@mui/x-date-pickers-pro/SingleInputDateRangeField';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { FormControl, MenuItem, Select } from "@mui/material";
+import { useState } from "react";
 
 export default function Slider() {
+    const [value, setValue] = useState('');
+
     return (
         <Box sx={{ mt: "110px", height: "600px", width: "100%" }}>
             <img src="https://easybook.demotheme.matbao.support/wp-content/uploads/2018/10/22.jpg" alt="slider-image" style={{ width: "100%", height: "600px", objectFit: "cover" }} />
@@ -29,38 +39,49 @@ export default function Slider() {
                     <Typography sx={{ fontSize: "44px", fontWeight: "700", fontFamily: "Nunito,sans-serif", color: "white" }}>Hệ Thống Đặt Phòng Khách Sạn EasyBook </Typography>
                     <Box width="5%" bgcolor="#F9B90F" height="4px" borderRadius="2px" m="15px" />
                     <Typography variant="h6" sx={{ color: "white", mb: "30px" }}>Hãy bắt đầu khám phá Việt Nam cùng EasyBook</Typography>
-                    <Box width="75%" maxWidth="1140px" bgcolor="#EEE" height="50px" border="10px solid #7B6F65" borderRadius="10px" display="flex" alignItems="center" justifyContent="space-between">
-                        <Box display="flex" alignItems="center" borderRight="#ccc solid 1px" height="100%">
-                            <NotListedLocationOutlinedIcon fontSize="small" sx={{ color: "#F9B90F", pl: 2, pr: 2 }} />
-                            <input placeholder="Khách sạn, Thành phố..." style={{ display: "flex", fontSize: "15px", border: "none", backgroundColor: "#EEE", height: "100%", flex: 1, padding: 0 }} />
-                        </Box>
-                        <Box display="flex" alignItems="center" borderRight="#ccc solid 1px" height="100%">
-                            <EventAvailableOutlinedIcon fontSize="small" sx={{ color: "#F9B90F", pl: 2, pr: 2 }} />
-                            {/* <TextField placeholder="Ngày đến - Ngày đi..."
-                                InputProps={{
-                                    style: {
-                                        fontSize: "13px",
-                                        border: "none"
-                                    }
-                                }} /> */}
-                            <input placeholder="Ngày đến - Ngày đi..." style={{ display: "flex", fontSize: "15px", border: "none", backgroundColor: "#EEE", height: "100%", flex: 1, padding: 0 }} />
-                        </Box>
-                        <Box display="flex" alignItems="center" borderRight="#ccc solid 1px" height="100%">
-                            <Groups2OutlinedIcon fontSize="small" sx={{ color: "#F9B90F", pl: 2, pr: 2 }} />
-                            {/* <TextField placeholder="Người lớn, phòng..."
-                                InputProps={{
-                                    style: {
-                                        fontSize: "13px",
-                                        borderColor: "#EEE"
-                                    }
-                                }} /> */}
-                            <input placeholder="Người lớn, phòng..." style={{ display: "flex", fontSize: "15px", border: "none", backgroundColor: "#EEE", height: "100%", flex: 1, padding: 0 }} />
-                        </Box>
-                        <Box display="flex" height="100%" alignItems="center" sx={{ backgroundColor: "#F9B90F", p: "0 30px" }}>
-                            <Typography sx={{ fontSize: "13px", color: "white" }}>Tìm kiếm</Typography>
-                            <ZoomInOutlinedIcon fontSize="small" sx={{ color: "white", pl: 1 }} />
-                        </Box>
+                    <Box width="75%" bgcolor="rgba(255, 255, 255, 0.25)" borderRadius="10px">
+                        <Box maxWidth="1140px" bgcolor="#EEE" height="50px" display="flex" alignItems="center" justifyContent="space-between" m="10px">
+                            <Box display="flex" alignItems="center" borderRight="#ccc solid 1px" height="100%" sx={{ minWidth: "250px" }}>
+                                <NotListedLocationOutlinedIcon fontSize="small" sx={{ color: "#F9B90F", pl: 2, pr: 2 }} />
+                                <Input disableUnderline sx={{ fontSize: "13px", flex: "1" }} placeholder="Khách sạn, Thành phố..." />
+                            </Box>
+                            <Box display="flex" alignItems="center" borderRight="#ccc solid 1px" height="100%" sx={{ minWidth: "250px" }} justifyContent="space-between">
+                                <EventAvailableOutlinedIcon fontSize="small" sx={{ color: "#F9B90F", pl: 2, pr: 2 }} />
+                                {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <DemoContainer components={['SingleInputDateRangeField']} >
+                                        <DateRangePicker
+                                            slots={{ field: SingleInputDateRangeField }}
+                                            name="allowedRange"
+                                        />
+                                    </DemoContainer>
+                                </LocalizationProvider> */}
+                            </Box>
+                            <Box display="flex" alignItems="center" borderRight="#ccc solid 1px" height="100%" sx={{ minWidth: "250px" }} >
+                                <Groups2OutlinedIcon fontSize="small" sx={{ color: "#F9B90F", pl: 2, pr: 2 }} />
+                                <FormControl variant="standard" sx={{ flex: 1, mr: 1, backgroundColor: "#EEE" }}>
+                                    <Select disableUnderline value={value} label="Child" sx={{ fontSize: "13px", flex: "1", height: "100%", backgroundColor: "#EEE" }}>
+                                        <MenuItem value="Người lớn" sx={{display:"flex", alignItems:"center", justifyContent:"space-between"}}>
+                                            <Typography fontSize="13px">Người lớn</Typography>
+                                            <TextField type="number" sx={{width:"70px"}}></TextField>
+                                        </MenuItem>
+                                        <MenuItem value="Trẻ em" sx={{display:"flex", alignItems:"center", justifyContent:"space-between"}}>
+                                            <Typography fontSize="13px">Trẻ em</Typography>
+                                            <TextField type="number" sx={{width:"70px"}}></TextField>
+                                        </MenuItem>
+                                        <MenuItem value="Phòng" sx={{display:"flex", alignItems:"center", justifyContent:"space-between"}} >
+                                            <Typography fontSize="13px">Phòng</Typography>
+                                            <TextField type="number" sx={{width:"70px"}}></TextField>
+                                        </MenuItem>
+                                    </Select>
+                                </FormControl>
 
+                            </Box>
+                            <Box display="flex" flex="1" height="100%" alignItems="center" justifyContent="center" sx={{ backgroundColor: "#F9B90F", m: "0 auto" }}>
+                                <Typography sx={{ fontSize: "13px", color: "white" }}>Tìm kiếm</Typography>
+                                <ZoomInOutlinedIcon fontSize="small" sx={{ color: "white", pl: 1 }} />
+                            </Box>
+
+                        </Box>
                     </Box>
 
                 </Box>
